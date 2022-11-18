6fd1e61ec1215c9b52b61c52be665fca40621d92
AntonObersteiner <anton.obersteiner@gmail.com>
2022-11-18 16:55:48 +0100

    Mehr Erklärungen bei der turtle

--- the_turtle.py
+++ the_turtle.py
@@ -1,14 +1,17 @@
 import turtle
 from math import sin, cos, pi
 
-#wohin mit dem pendown, damit keine Linie von (0, 0) nach oben führt?
-turtle.penup() #setzt Stift auf
-turtle.pendown() #hebt Stift an
+# die Turtle beginnt immer bei (0, 0), also im Zentrum
+# wohin mit dem pendown, damit bei dem Stern keine Linie 
+# von (0, 0) nach oben führt?
+turtle.penup() #setzt Stift auf/beginnt zu zeichnen
+turtle.pendown() #hebt Stift an/zeichnet erstmal nicht weiter
 
 
 def draw_star(points):
+    """soll Stern mit <points> Spitzen zeichnen, malt aber immer einen mit 5"""
 	turtle.fillcolor(.5, .1, .9)
-	turtle.begin_fill()
+	turtle.begin_fill() # beginnt Füll-Modus
 
 	for i in range(0, 6):
 	    angle = i * 2 * pi / 5 * 2
@@ -16,6 +19,8 @@ def draw_star(points):
 	    y = 200 * cos(angle)
 	    turtle.goto(x, y)
 
+    # hier wird der Füll-Modus beendet, sodass alles, 
+    # was dazwischen gezeichnet wurde, gefüllt wird
 	turtle.end_fill()
 
 if __name__ == '__main__':
